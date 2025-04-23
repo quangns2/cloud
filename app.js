dbae5f76464c28c68f3c2e02b1a11c6812b54684
quangns2 <nutigameming@gmail.com>
2025-04-23 10:27:36 +0700

    update1

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Kết nối MongoDB
-mongoose.connect('mongodb://localhost/vocab-builder', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Error connecting to MongoDB:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
 
 // Định nghĩa routes
 const vocabRoutes = require('./routes/vocabRoutes');
